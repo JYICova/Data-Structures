219bf7978d09279149240032c547d83adff3b335
Shaleel Cova <shaleelcova@live.com>
2022-07-12 15:28:43 +0200

    Hello-world created

---
+++ Hello-World.cpp
@@ -0,0 +1,5 @@
+#include <iostream> //Preprocessing Directive for iostream
+
+int main(){ //main function is called when program is executed
+    std::cout << "Hello World" << std::endl; // Print
+} // default return value is 0
