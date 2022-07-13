96e17463e2b0efaa657d60f1c8e92e43ac693384
Shaleel Cova <shaleelcova@live.com>
2022-07-13 16:21:08 +0200

    File update

--- Arrays/vector.cpp
+++ Arrays/vector.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
 
-int main(){
-	int num = 5;
-	std::cout << num << std::endl;
-	return 0;
-}
+
+#include <iostream>
+using namespace std;
+int main()
+	{
+		int const a[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 4};
+		int length;
+		length = sizeof(a)/sizeof(int);
+		cout << length << endl;
+		cout << a[0] << endl;
+		for(size_t i = length; i--;)
+			cout << a[i];
+		//for(size_t i = numElements; i--;)
+	}
