13dc5a932f40b0c2cb50737012e19df76cb31877
Shaleel Cova <shaleelcova@live.com>
2022-07-16 19:23:37 +0200

    a

--- Arrays/vector.cpp
+++ Arrays/vector.cpp
@@ -1,35 +1,32 @@
-
-
+// C++ program to illustrate the
+// iterators in vector
 #include <iostream>
+#include <vector>
+  
 using namespace std;
+  
 int main()
-	{
-		int a[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 4};
-		int length;
-		length = sizeof(a)/sizeof(int);
-		cout << length << endl;
-		cout << a[0] << endl;
-		for(int i = 0; i < length; i++)
-			cout << a[i] << ", ";
-		//for(size_t i = numElements; i--;)
-
-		// input an index to read from array
-		int i;
-		cout << "\nEnter Index: ";
-		cin >> i;
-		cout << a[i] << endl;
-		a[0] = 1;
-		cout << "Press a key to continue...";
-		cin.get();
-		cin.get();
-		// do 
-		//  {
-		//    cout << '\n' << "Press a key to continue...";
-		//  } while {}
-		//  (cin.get() != '\n');
-		// }
-
-
-	}
-
-
+{
+    vector<int> g1;
+  
+    for (int i = 1; i <= 5; i++)
+        g1.push_back(i);
+  
+    cout << "Output of begin and end: ";
+    for (vector<int>::iterator i = g1.begin(); i != g1.end(); ++i)
+        cout << *i << " ";
+  
+    cout << "\nOutput of cbegin and cend: ";
+    for (vector<int>::const_iterator i = g1.cbegin(); i != g1.cend(); ++i)
+        cout << *i << " ";
+  
+    cout << "\nOutput of rbegin and rend: ";
+    for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir)
+        cout << *ir << " ";
+  
+    cout << "\nOutput of crbegin and crend : ";
+    for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir)
+        cout << *ir << " ";
+  
+    return 0;
+}
